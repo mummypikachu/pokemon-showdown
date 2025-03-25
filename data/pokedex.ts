c16a092c56d7f6a3d898371a804e733934030a69
dukefrogpeasant <yaxinjp@gmail.com>
2025-03-25 21:41:24 +0100

    Fix Avalugg ability1

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -12799,7 +12799,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		name: "Avalugg",
 		types: ["Ice"],
 		baseStats: {hp: 95, atk: 117, def: 184, spa: 24, spd: 46, spe: 28},
-		abilities: {0: "Snowscape", 1: "Ice Body", H: "Sturdy"},
+		abilities: {0: "Snow Warning", 1: "Ice Body", H: "Sturdy"},
 		heightm: 2,
 		weightkg: 505,
 		color: "Blue",
