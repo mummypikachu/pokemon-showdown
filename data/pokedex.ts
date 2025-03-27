a686286e41b3594cceee5e7817aa0fb26c2bb97b
dukefrogpeasant <yaxinjp@gmail.com>
2025-03-27 20:57:06 +0100

    back in line to +100 BST from pidgeot

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -397,7 +397,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		baseSpecies: "Pidgeot",
 		forme: "Mega",
 		types: ["Normal", "Flying"],
-		baseStats: {hp: 83, atk: 80, def: 85, spa: 105, spd: 85, spe: 121},
+		baseStats: {hp: 83, atk: 80, def: 85, spa: 125, spd: 85, spe: 121},
 		abilities: {0: "No Guard"},
 		heightm: 2.2,
 		weightkg: 50.5,
