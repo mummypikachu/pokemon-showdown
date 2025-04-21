f4c122e5bd8122b47f367afe8dc70337f785393c
dukefrogpeasant <yaxinjp@gmail.com>
2025-04-21 18:38:34 +0200

    A LOT of movepools
    A LOT of pokemon gaining a expansion on movepool.
    
    Some straight up getting over 30 new moves.

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -6212,7 +6212,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		weightkg: 82,
 		color: "Green",
 		eggGroups: ["Bug", "Dragon"],
-		requiredItem: "Cameruptite",
+		requiredItem: "Cameruptite", //placeholder
 	},
 	cacnea: {
 		num: 331,
