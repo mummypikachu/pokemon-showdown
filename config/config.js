bdbbc3544d2c262d12539ffc941734007e0a6cab
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2025-08-15 19:22:10 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -90,7 +90,7 @@ Main's SSL deploy script from Let's Encrypt looks like:
  *   know what you are doing
  * @type {false | string[]}.
  */
-exports.proxyip = false;
+exports.proxyip = ["127.0.0.1"];
 
 /**
  * Various debug options
@@ -459,7 +459,7 @@ exports.appealurl = '';
  * replsocketprefix - the prefix for the repl sockets to be listening on
  * replsocketmode - the file mode bits to use for the repl sockets
  */
-exports.repl = true;
+exports.repl = false;
 exports.replsocketprefix = './logs/repl/';
 exports.replsocketmode = 0o600;
 
