7c9519f5e2f6a3d90ad1f08b2d59ac919df6db51
mummypikachu <kymeerhh@gmail.com>
2026-03-27 18:35:42 -0400

    saving replays

--- server/chat-plugins/saveserverreplay.ts
+++ server/chat-plugins/saveserverreplay.ts
@@ -58,7 +58,7 @@ export const commands: Chat.ChatCommands = {
 				`<script type="text/plain" class="battle-log-data">${data}</script>\n` +
 				'</div>\n' +
 				'<script>\n' +
-				`let daily = Math.floor(Date.now()/1000/60/60/24);document.write('<script src="http://play.sigmatic-showdown.online/js/replay-embed.js?version'+daily+'"></'+'script>');\n` +
+				`let daily = Math.floor(Date.now()/1000/60/60/24);document.write('<script src="https://play.sigmatic-showdown.online/js/replay-embed.js?version'+daily+'"></'+'script>');\n` +
 				'</script>\n'
 			);
 			out.end();
