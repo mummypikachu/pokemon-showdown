795e3f83c21165673ec4dc8e47c67d95abccf45a
dukefrogpeasant <yaxinjp@gmail.com>
2026-03-29 00:22:19 +0100

    the item description

--- data/text/items.ts
+++ data/text/items.ts
@@ -1827,6 +1827,10 @@ export const ItemsText: {[k: string]: ItemText} = {
 		name: "Heatranite",
 		desc: "If held by a Heatran, this item allows it to Mega Evolve in battle.",
 	},
+	celebinite: {
+		name: "celebinite",
+		desc: "If held by a Celebi, this item allows it to Mega Evolve in battle.",
+	},
 	clawitzernite: {
 		name: "clawitzernite",
 		desc: "If held by a Clawitzer, this item allows it to Mega Evolve in battle.",
