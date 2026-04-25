5876d0feac7006ba0a0f7cbf858eccb403a3b38f
dukefrogpeasant <yaxinjp@gmail.com>
2026-04-26 01:05:23 +0200

    Update formats-data.ts

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -6135,6 +6135,9 @@ export const FormatsData: { [k: string]: SpeciesFormatsData; } = {
 		tier: "UUBL",
 		newDexTier: "Uber",
 	},
+	matriall: {
+		tier: "LC",
+	},
 	scaffixer: {
 		tier: "OU",
 	},
