4ecd84c489c74c3eb87b15e2d2b2a4e8d111f0a8
dukefrogpeasant <yaxinjp@gmail.com>
2026-04-26 23:00:27 +0200

    Try to fix the Light Ball

--- data/items.ts
+++ data/items.ts
@@ -3423,13 +3423,13 @@ export const Items: {[itemid: string]: ItemData} = {
 		onModifyAtkPriority: 1,
 		onModifyAtk(atk, pokemon) {
 			if (pokemon.baseSpecies.baseSpecies === 'Pikachu')  {
-				return this.chainModify(2);
+				return this.chainModify(2.0);
 			}
 		},
 		onModifySpAPriority: 1,
 		onModifySpA(spa, pokemon) {
 			if (pokemon.baseSpecies.baseSpecies === 'Pikachu') {
-				return this.chainModify(2);
+				return this.chainModify(2.0);
 			}
 		},
 		itemUser: ["Pikachu", "Pikachu-Cosplay", "Pikachu-Rock-Star", "Pikachu-Belle", "Pikachu-Pop-Star", "Pikachu-PhD", "Pikachu-Libre", "Pikachu-Original", "Pikachu-Hoenn", "Pikachu-Sinnoh", "Pikachu-Unova", "Pikachu-Kalos", "Pikachu-Alola", "Pikachu-Partner", "Pikachu-Starter", "Pikachu-World", "Pikachu-Windychu", "Pikachu-Wavychu"],
