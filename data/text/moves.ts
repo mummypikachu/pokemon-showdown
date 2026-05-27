4548e7debb1287ed2f3e08227ce01212e3ff67f1
dukefrogpeasant <yaxinjp@gmail.com>
2026-05-28 00:30:02 +0200

    fix infect name, descs scaletip/bis

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -7880,12 +7880,20 @@ export const MovesText: {[k: string]: MoveText} = {
 		shortDesc: "Scaffixer: stronger on forms, changes to Scaffixer.",
 	},
 	infect: {
-		name: "Worry Seed",
+		name: "Infect",
 		desc: "Causes the target's Ability to become Infective. Fails if the target's Ability is As One, Battle Bond, Comatose, Commander, Disguise, Gulp Missile, Hadron Engine, Ice Face, Infective, Multitype, Orichalcum Pulse, Power Construct, Protosynthesis, Quark Drive, RKS System, Schooling, Shields Down, Stance Change, Truant, Zen Mode, or Zero to Hero.",
 		shortDesc: "The target's Ability becomes Infective.",
 	},
 	manelash: {
 		name: "Mane Lash",
 		shortDesc: "Can't be selected turn after use. 30% burn.",
+	},
+	scaletip: {
+		name: "Scale Tip",
+		shortDesc: " PLACEHOLDER Category based on primary type of opponent in gen 3.",
+	},
+	bis: {
+		name: "Bis",
+		shortDesc: " PLACEHOLDER Encores the target into their last used move.",
 	},
 };
