03e4b3398118fabafdb705da96b460e8b5387430
dukefrogpeasant <yaxinjp@gmail.com>
2026-09-06 01:09:49 +0200

    Revert "Demolish rands (FOR NOW), for testing purpose"
    This reverts commit b7865c8ae1425f5a00919317eb83150b865d03f0.

--- data/random-teams.ts
+++ data/random-teams.ts
@@ -966,7 +966,8 @@ export class RandomTeams {
 		if (abilities.has('Guts') && (moves.has('facade') || moves.has('sleeptalk'))) return 'Guts';
 		if (abilities.has('Harvest') && moves.has('substitute')) return 'Harvest';
 		if (species.id === 'hypno') return 'Iron Fist';
-		//below is a list of a few Mega pokemon that have to have their abilities forced to a normal one.
+		//below is a list of Mega pokemon that have to have their abilities forced to a normal one.
+		if (species.id === 'garchompmegaz') return 'Rough Skin';
 		if (species.id === 'lucariomegaz') return 'Inner Focus';
 		if (species.id === 'flygonmegaz') return 'Levitate';
 		if (species.id === 'regigigasmega') return 'Slow Start';
