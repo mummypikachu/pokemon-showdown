5c5931c98b0774e1638a8e6715e44cf79b625d67
dukefrogpeasant <yaxinjp@gmail.com>
2026-09-19 18:19:34 +0200

    Add clause for Zygarde to use Zygardite in MNM

--- config/formats.ts
+++ config/formats.ts
@@ -669,15 +669,23 @@ export const Formats: FormatList = [
 		mod: 'mixandmega',
 		ruleset: ['Standard OMs', 'Evasion Items Clause', 'Evasion Abilities Clause', 'Sleep Moves Clause', 'Min Source Gen = 9'],
 		banlist: ['Koraidon', 'Miraidon', 'Beedrillite', 'Blazikenite', 'Gengarite', 'Kangaskhanite', 'Mawilite', 'Medichamite', 'Moody', 'Rusted Sword',
-			'Shadow Tag', 'Baton Pass', 'Shed Tail', 'Mothimite', 'Melmetalite', 'Talonflamite', 'Celebinite', 'Zygardite', 'Starminite', 'Scovillainite',
+			'Shadow Tag', 'Baton Pass', 'Shed Tail', 'Mothimite', 'Talonflamite', 'Celebinite', 'Starminite', 'Scovillainite', 'Melmetalite',
 			'Raichunite Y', 'Malamarite', 'Orbeetite'],
 		restricted: [
 			'Arceus', 'Calyrex-Ice', 'Ceruledge', 'Deoxys-Normal', 'Deoxys-Attack', 'Dialga', 'Eternatus', 'Flutter Mane', 'Gholdengo', 'Giratina', 'Gouging Fire',
 			'Groudon', 'Ho-Oh', 'Iron Bundle', 'Kyurem-Black', 'Kyurem-White', 'Lugia', 'Lunala', 'Manaphy', 'Mewtwo', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane',
 			'Palkia', 'Rayquaza', 'Regigigas', 'Reshiram', 'Slaking', 'Sneasler', 'Solgaleo', 'Ursaluna-Bloodmoon', 'Urshifu-Single-Strike', 'Walking Wake', 'Zacian',
-			'Zekrom', 'Melmetal + Melmetalite', 'Evolotto'
+			'Zekrom', 'Evolotto',
 		],
 		onValidateTeam(team) {
+			for (const set of team) {
+				const item = this.dex.items.get(set.item);
+				const species = this.dex.species.get(set.species);
+
+				if (item.id === 'zygardite' && species.baseSpecies !== 'Zygarde') {
+					return [`${species.name} cannot hold Zygardite. (Only Zygarde-Complete can use the Zygardite.)`];
+				}
+			}
 			const itemTable = new Set<ID>();
 			for (const set of team) {
 				const item = this.dex.items.get(set.item);
